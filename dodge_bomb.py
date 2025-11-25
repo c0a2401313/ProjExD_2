1625163cb4932e780b9bf1cacb403d79b80fe1b3
ikeda <c0a2401313@edu.teu.ac.jp>
2025-11-25 14:13:17 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,14 +1,14 @@
 import os
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
     pg.K_UP:(0,-5),
     pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(+5,0),
-    pg.K_RIGHT:(-5,0)
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0)
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,7 +21,11 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10,10))
+    pg.draw.circle(bb_img,(255,0,0),(10,10,10),10)
+    bb_img.set_colorkey(0,0,0)
+    bb_rct=bb_img.get_rect()
+    bb_rct.centerx=random.randint(0,WIDTH)
+    bb_rct.centery=random.randint(0,HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -30,16 +34,16 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        #key_lst = pg.key.get_pressed()
+        # sum_mv = [0, 0]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
